dbd7f9f313400f08f588f512ceea9dd350347333
Colin <cmmatt14@gmail.com>
2023-03-15 19:15:18 -0400

    hompage render

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,13 @@
 import React from "react";
+import ParticipantAmountForm from "./TournamentSetup/ParticipantAmountForm";
 
-const Homepage = () => {};
+const Homepage = () => {
+  return (
+    <div>
+      <h2>Tournament Bracket</h2>
+      <ParticipantAmountForm />
+    </div>
+  );
+};
 
 export default Homepage;
